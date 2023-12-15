0d44c38a99d7158e77dc4370cfeaf1da885d4240
jonell888 <144983408+jonell888@users.noreply.github.com>
2023-12-15 13:41:55 +0700

    Update joejoe.py

--- joejoe.py
+++ joejoe.py
@@ -4,3 +4,11 @@ print("My nickname Joe")
 print("Graduated from Mathayom 6")
 print("Graduated from That Phanom School")
 print("Nakhon Phanom Province")
+
+print("Currently studying in 3rd year.")
+print("Information Technology branch")
+print("Faculty of Management")
+print("Nakhon Phanom University")
+print("In the future I want to work as software developer")
+
+
